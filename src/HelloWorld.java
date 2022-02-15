6148ffe89608bb944c791a42321bfd5c7e859fcb
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-02-15 16:00:56 -0600

    completed syntax type and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,7 +14,58 @@ public class HelloWorld {
 
         //print out "hello world" to the console
         System.out.println("Hello World");
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+
+        int myFavoriteNumber = 36;
+        String myString = "Hello Codeup";
+
+        System.out.println(myString);
+
+//        float myNumber = 3.14;
+
+//        System.out.println(myNumber);
+        //outputs the x value before its incremented and then prints the incremented number
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        // increments first and then prints the new number for both prints
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        //doesn't work because class is a keyword
+//        int class = 4;
+
+        //creates a runtime error
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+
+        //strings cannot be converted to ints
+//        int three = (int) "three";
+
+        //top issue is a runtime error because no syntax was wrong but the logic doesn't make sense
+        //the bottom issue states a string cannot be converted into an int
+
+//        int x = 4;
+//        System.out.println(x =+ 5);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        //this will first divide x and y and then y - x
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        //the number is incremented over what it can hold will cycle through until what its allowed to hold.
 
     }
 
