944a372c8f7e8f926a8b47f364216284056c34f2
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-02-17 09:24:01 -0600

    console exercise review

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -18,6 +18,7 @@ public class ConsoleExercise {
         String string2;
         String string3;
 
+
         //Prompt
         System.out.print("Enter 3 words: ");
 
@@ -26,8 +27,52 @@ public class ConsoleExercise {
         string2 = scanner1.next();
         string3 = scanner1.next();
 
+        //Console Output
         System.out.println("This is what you entered: ");
-        System.out.println(string1 + ", " + string2 + ", and " + string3);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+
+        //when entering more than 3 words the system will still run but only output the first 3 words, but when less the 3 is entered the system will not respond but wont return an error
+
+        //Declarations
+        Scanner scanner2 = new Scanner(System.in);
+        String sentence;
+
+        //Prompt
+        System.out.print("Enter a sentence: ");
+
+        //Entered Values
+        sentence = scanner2.nextLine();
+
+        //Console Output
+        System.out.println("Here is your sentence: ");
+        System.out.println(sentence);
+
+        //When using scanner.next() only receiving the first letter of the sentence. When using scanner.nextLine() I was able to return the whole sentence
+
+        //Declarations
+        Scanner areaCalculator = new Scanner(System.in);
+        String length;
+        String width;
+
+        //Prompt 1
+        System.out.print("Enter the length of your room: ");
+
+        //Entered value
+        length = areaCalculator.nextLine();
+
+        //Prompt 2
+        System.out.print("Now enter the width: ");
+
+        //Entered value
+        width = areaCalculator.nextLine();
+
+
+
+
+
+
 
     }
 }
