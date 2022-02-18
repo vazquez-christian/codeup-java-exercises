1ab8bea8afbd00db207fc89459062b1b16147e99
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-02-18 10:29:29 -0600

    completed StringExercise moving to Bob exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String string1 = "We don't need no education";
+        System.out.println(string1);
+
+        String string2 = "We don't need no thought control";
+        System.out.println(string2);
+
+        String string3 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(string3);
+
+        String string4 = "In windows, the maine drive is usually C:\\";
+        System.out.println(string4);
+
+        String string5 = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\!";
+        System.out.println(string5);
+        //need to double escape each backslash
+    }
+}
