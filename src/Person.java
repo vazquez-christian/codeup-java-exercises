7fa27d556975a16166ac8fdefc26cb517ffb541c
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-02-22 15:01:14 -0600

    updating OOP exericises

--- src/Person.java
+++ src/Person.java
@@ -1,25 +1,45 @@
 public class Person {
-    //Two String data types
-    public String firstName;
-    public String lastName;
-    //Extra examples: More fields for a Person to have when instantiated (created)
-    private int age;
-    public String relationshipStatus;
+    private String firstName;
 
-    //Properties can also be METHODS, e.g.
-    public String sayHello() {
-        return String.format("Hello from %s %s", firstName, lastName);
+    public String getName() {
+        return this.firstName;
+    }
+
+    public void setName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello from " + this.firstName + "!");
+    }
+
+    public Person (String name) {
+        this.firstName = name;
     }
 
     public static void main(String[] args) {
-        //CREATED AN INSTANCE OF A PERSON
-        Person kenneth = new Person();
-        kenneth.firstName = "Kenneth";
-        kenneth.lastName = "Howell";
-        kenneth.age = 34;
-        kenneth.relationshipStatus = "taken";
-        System.out.println(kenneth.firstName);
-        System.out.println("sayHello() = " + kenneth.sayHello());
-    }
+        Person newUser = new Person("Christian");
+//        newUser.sayHello();
+        newUser.setName("Carlos");
+        newUser.sayHello();
+        System.out.println(newUser.getName());
 
+//        Person person1 = new Person("John"); //Creates a new person named John
+//        Person person2 = new Person("John"); //Creates another new person also named John
+//        System.out.println(person1.getName().equals(person2.getName())); // Going to return just one John... // returned true
+//        System.out.println(person1 == person2); //Going to return person1 and person2 or true... // returned false
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1; //assigning the person1 as person2 as well wil not change it, its just two variables for the same thing.. its redundant
+//        System.out.println(person1 == person2); // Going to return true because the statement will always be true
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName()); //Will print John
+        System.out.println(person2.getName()); //Will also print John
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
 }
+
